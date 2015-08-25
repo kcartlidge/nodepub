3ac4ce81b84394f4ddfc1099a140e1cef4dd3619
K Cartlidge <kcartlidge@users.noreply.github.com>
2015-08-25 07:38:40 +0100

    Noddy test to confirm the testing infrastructure is in place.

---
+++ test/test.basics.js
@@ -0,0 +1,9 @@
+
+var expect = require('chai').expect;
+
+describe("The testing framework and assertion library", function() {
+  it("should be active", function() {
+    expect(true).to.equal(true);
+  });
+});
+
