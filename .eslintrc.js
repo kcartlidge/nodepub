f1feb8cf5ca589555a88c4a948568e7e3c1189aa
K Cartlidge <code@kcartlidge.com>
2021-02-17 21:29:25 +0000

    Updated eslint and tidied code

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    es2021: true,
+    mocha: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+};
--- example/example.js
+++ example/example.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
-var nodepub = require("../src/index.js"),
-  _ = require("lodash");
+const _ = require('lodash');
+const nodepub = require('../src/index.js');
 
 // Metadata example.
-var metadata = {
+const metadata = {
   id: '278-123456789',
   cover: 'test/test-cover.png',
   title: 'Unnamed Document',
@@ -20,78 +20,78 @@ var metadata = {
   description: 'A test book.',
   contents: 'Chapters',
   source: 'http://www.kcartlidge.com',
-  images: ['test/hat.png']
+  images: ['test/hat.png'],
 };
 
-var copyright = `<h1>[[TITLE]]</h1>
+const copyright = `<h1>[[TITLE]]</h1>
 <h2>[[AUTHOR]]</h2>
 <h3>&copy; [[COPYRIGHT]]</h3>
 <p>
-	All rights reserved.
+  All rights reserved.
 </p>
 <p>
-	No part of this book may be reproduced in any form or by any electronic or
-	mechanical means, including information storage and retrieval systems, without
-	written permission from the author, except where covered by fair usage or
-	equivalent.
+  No part of this book may be reproduced in any form or by any electronic or
+  mechanical means, including information storage and retrieval systems, without
+  written permission from the author, except where covered by fair usage or
+  equivalent.
 </p>
 <p>
-	This book is a work of fiction.
-	Any resemblance to actual persons	(living or dead) is entirely coincidental.
+  This book is a work of fiction.
+  Any resemblance to actual persons  (living or dead) is entirely coincidental.
 </p>
 `;
 
-var more = `<h1>More Books to Read</h1>
+const more = `<h1>More Books to Read</h1>
 <h2>Thanks for reading <em>[[TITLE]]</em>.</h2>
 <p>
-	I hope you enjoyed the book, but however you felt please consider leaving a
-	review where you purchased it and help other readers discover it.
+  I hope you enjoyed the book, but however you felt please consider leaving a
+  review where you purchased it and help other readers discover it.
 </p>
 <p>
-	You can find links to more books (and often special offers/discounts) by
-	visiting my site at <a href="http://kcartlidge.com/books">KCartlidge.com/books</a>.
+  You can find links to more books (and often special offers/discounts) by
+  visiting my site at <a href="http://kcartlidge.com/books">KCartlidge.com/books</a>.
 </p>
 `;
 
-var about = `<h1>About the Author</h1>
+const about = `<h1>About the Author</h1>
 <p>
-	This is just some random blurb about the author.
+  This is just some random blurb about the author.
 </p>
 <p>
-	You can find more at the author's site.
+  You can find more at the author's site.
 </p>
 <p>
-	Oh, and here's a picture of a hat:
+  Oh, and here's a picture of a hat:
 </p>
 <p>
-	<img src="../images/hat.png" alt="A hat." />
+  <img src="../images/hat.png" alt="A hat." />
 </p>
 `;
 
 // Dummy text (lorem ipsum).
-var lipsum = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis iaculis pharetra. Proin malesuada tortor ut nibh viverra eleifend.</p><p>Duis efficitur, arcu vitae viverra consectetur, nisi mi pharetra metus, vel egestas ex velit id leo. Curabitur non tortor nisi. Mauris ornare, tellus vel fermentum suscipit, ligula est eleifend dui, in elementum nunc risus in ipsum. Pellentesque finibus aliquet turpis sed scelerisque. Pellentesque gravida semper elit, ut consequat est mollis sit amet. Nulla facilisi.</p>";
-for (var i = 0; i < 3; i++) {
-  lipsum = lipsum + lipsum;
+let lipsum = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis iaculis pharetra. Proin malesuada tortor ut nibh viverra eleifend.</p><p>Duis efficitur, arcu vitae viverra consectetur, nisi mi pharetra metus, vel egestas ex velit id leo. Curabitur non tortor nisi. Mauris ornare, tellus vel fermentum suscipit, ligula est eleifend dui, in elementum nunc risus in ipsum. Pellentesque finibus aliquet turpis sed scelerisque. Pellentesque gravida semper elit, ut consequat est mollis sit amet. Nulla facilisi.</p>';
+for (let i = 0; i < 3; i += 1) {
+  lipsum += lipsum;
 }
 
 // Optional override to replace auto-generated contents page.
 // If not required, just drop it from the 'var epub=' call below.
-var generateContentsPage = function (links) {
-  var contents = "<h1>Chapters</h1>";
-  _.each(links, function (link) {
+const generateContentsPage = (links) => {
+  let contents = '<h1>Chapters</h1>';
+  _.each(links, (link) => {
     // Omit all but the main pages.
-    if (link.itemType === "main") {
-      if (link.title === "More Books to Read") {
-        contents += "<div> &nbsp;</div>";
+    if (link.itemType === 'main') {
+      if (link.title === 'More Books to Read') {
+        contents += '<div> &nbsp;</div>';
       }
-      contents += "<div><a href='" + link.link + "'>" + link.title + "</a></div>";
+      contents += `<div><a href='${link.link}'>${link.title}</a></div>`;
     }
   });
   return contents;
 };
 
 // Set up the EPUB basics.
-var epub = nodepub.document(metadata, generateContentsPage);
+const epub = nodepub.document(metadata, generateContentsPage);
 epub.addCSS(`body { font-family:Verdana,Arial,Sans-Serif; font-size:11pt; }
 #title,#title h1,#title h2,#title h3 { text-align:center; }
 h1,h3,p { margin-bottom:1em; }
@@ -104,18 +104,18 @@ epub.addSection('Title Page', "<div id='title'><h1>[[TITLE]]</h1><h2>Book <stron
 epub.addSection('Copyright', copyright, false, true);
 
 // Add some content.
-epub.addSection('Chapter 1', "<h1>One</h1>" + lipsum);
-epub.addSection('Chapter 2', "<h1>Two</h1>" + lipsum);
-epub.addSection('Chapter 2a', "<h1>Two (A)</h1><p><strong>This chapter does not appear in the contents.</strong></p>" + lipsum, true);
-epub.addSection('Chapter 3', "<h1>Three</h1><p>Here is a sample list.</p><ul><li>Sample list item one.</li><li>Sample list item two.</li><li>Sample list item three.</li></ul>" + lipsum);
+epub.addSection('Chapter 1', `<h1>One</h1>${lipsum}`);
+epub.addSection('Chapter 2', `<h1>Two</h1>${lipsum}`);
+epub.addSection('Chapter 2a', `<h1>Two (A)</h1><p><strong>This chapter does not appear in the contents.</strong></p>${lipsum}`, true);
+epub.addSection('Chapter 3', `<h1>Three</h1><p>Here is a sample list.</p><ul><li>Sample list item one.</li><li>Sample list item two.</li><li>Sample list item three.</li></ul>${lipsum}`);
 epub.addSection('More Books to Read', more);
 epub.addSection('About the Author', about);
 
 // Generate the result.
 epub.writeEPUB(
-  function (e) { console.log("Error:", e); },
+  (e) => { console.log('Error:', e); },
   'example', 'example',
-  function () { console.log("See the 'example' subfolder.") }
+  () => { console.log("See the 'example' subfolder."); },
 );
 
 // Also write the structure both for debugging purposes and also to provide sample output in GitHub.
